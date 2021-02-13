7fd7c10c67b7451ec5b33b8406fe24c97d00a263
matheushent <matheustosta66@gmail.com>
2021-02-13 11:17:50 -0300

    prepare code for CDK

---
+++ app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from aws_cdk import core
+
+from stacks.api import Project
+
+
+env = core.Environment(
+    account = '065963581185',
+    region = 'sa-east-1'
+)
+
+app = core.App()
+Project(
+    app, Project.__name__, env=env,
+    tags = {
+        'Application': 'EEL8051',
+        'STAGE': 'dev'
+    }
+)
+
+app.synth()
